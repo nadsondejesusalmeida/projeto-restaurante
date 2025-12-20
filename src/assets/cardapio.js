aebb036be5683ff9b13a3e8d9697dc29a4949978
nadsondejesusalmeida <developer.brownnose603@slmails.com>
2025-12-20 11:15:49 -0300

    Pequenas mudanças

--- src/assets/cardapio.js
+++ src/assets/cardapio.js
@@ -15,108 +15,108 @@ import guaranaImage from "./drinks/guarana.jpeg";
 import heinekenImage from "./drinks/heineken.jpeg";
 
 const rolledMignon = {
-  name: "Enroladinho de Mignon",
-  price: "R$ 64,90",
-  image: rolledMignonImage,
-  description: "Finíssimas fatias de filé mignon enroladas no parmesão",
+	name: "Enroladinho de Mignon",
+	price: "R$ 64,90",
+	image: rolledMignonImage,
+	description: "Finíssimas fatias de filé mignon enroladas no parmesão",
 };
 
 const couple20 = {
-  name: "Casal 20",
-  price: "R$ 29,00",
-  image: couple20Image,
-  description: "Casal perfeito pão de alho caseiro e linguiça toscana grill",
+	name: "Casal 20",
+	price: "R$ 29,00",
+	image: couple20Image,
+	description: "Casal perfeito pão de alho caseiro e linguiça toscana grill",
 };
 
 const picanhaBurger = {
-  name: "Burguer de Picanha",
-  price: "R$ 44,90",
-  image: picanhaBurgerImage,
-  description:
-    "Burguer de Picanha Angus, Queijo Cheddar, Crisp de Cebola e Geléia de Bacon",
+	name: "Burguer de Picanha",
+	price: "R$ 44,90",
+	image: picanhaBurgerImage,
+	description:
+		"Burguer de Picanha Angus, Queijo Cheddar, Crisp de Cebola e Geléia de Bacon",
 };
 
 const fishAndChips = {
-  name: "Fish and Chips",
-  price: "R$ 79,90",
-  image: fishAndChipsImage,
-  description:
-    "Iscas de Peixe Empanada na Farinha Panko e Flocos de Milho e Batata Frita Palito",
+	name: "Fish and Chips",
+	price: "R$ 79,90",
+	image: fishAndChipsImage,
+	description:
+		"Iscas de Peixe Empanada na Farinha Panko e Flocos de Milho e Batata Frita Palito",
 };
 
 const truffleShrimpRisotto = {
-  name: "Risoto de Camarão Trufado",
-  price: "R$ 72,90",
-  image: truffleShrimpRisottoImage,
-  description:
-    "Arroz Arbóreo, camarões salteados no azeite trufado e crocantes de panko",
+	name: "Risoto de Camarão Trufado",
+	price: "R$ 72,90",
+	image: truffleShrimpRisottoImage,
+	description:
+		"Arroz Arbóreo, camarões salteados no azeite trufado e crocantes de panko",
 };
 
 const brownie = {
-  name: "Brownie",
-  price: "R$ 32,90",
-  image: brownieImage,
-  description:
-    "Delicioso brownie caseiro, sorvete de creme, calda quente de chocolate",
+	name: "Brownie",
+	price: "R$ 32,90",
+	image: brownieImage,
+	description:
+		"Delicioso brownie caseiro, sorvete de creme, calda quente de chocolate",
 };
 
 const bakedCoconut = {
-  name: "Cocada de Forno",
-  price: "R$ 32,90",
-  image: bakedCoconutImage,
-  description:
-    "Cremosa cocada de coco torrado, doce de leite argentino e sorvete de creme",
+	name: "Cocada de Forno",
+	price: "R$ 32,90",
+	image: bakedCoconutImage,
+	description:
+		"Cremosa cocada de coco torrado, doce de leite argentino e sorvete de creme",
 };
 
 const petitGateau = {
-  name: "Petit Gateau",
-  price: "R$ 34,90",
-  image: petitGateauImage,
-  description:
-    "Bolo quente recheado com ganache de chocolate, leite condensado e castanha de caju",
+	name: "Petit Gateau",
+	price: "R$ 34,90",
+	image: petitGateauImage,
+	description:
+		"Bolo quente recheado com ganache de chocolate, leite condensado e castanha de caju",
 };
 
 const stillWater = {
-  name: "Água sem gás",
-  price: "R$ 6,90",
-  image: stillWaterImage,
-  description: "310ml",
+	name: "Água sem gás",
+	price: "R$ 6,90",
+	image: stillWaterImage,
+	description: "310ml",
 };
 
 const sparklingWater = {
-  name: "Água com gás",
-  price: "R$ 7,90",
-  image: sparklingWaterImage,
-  description: "310ml",
+	name: "Água com gás",
+	price: "R$ 7,90",
+	image: sparklingWaterImage,
+	description: "310ml",
 };
 
 const cocaCola = {
-  name: "Coca-Cola",
-  price: "R$ 7,90",
-  image: cocaColaImage,
-  description: "290ml",
+	name: "Coca-Cola",
+	price: "R$ 7,90",
+	image: cocaColaImage,
+	description: "290ml",
 };
 
 const guarana = {
-  name: "Guaraná Antártica",
-  price: "R$ 7,90",
-  image: guaranaImage,
-  description: "350ml",
+	name: "Guaraná Antártica",
+	price: "R$ 7,90",
+	image: guaranaImage,
+	description: "350ml",
 };
 
 const heineken = {
-  name: "Heineken",
-  price: "R$ 15,90",
-  image: heinekenImage,
-  description: "355ml",
+	name: "Heineken",
+	price: "R$ 15,90",
+	image: heinekenImage,
+	description: "355ml",
 };
 
 export const mainDishes = [
-  picanhaBurger,
-  rolledMignon,
-  couple20,
-  fishAndChips,
-  truffleShrimpRisotto,
+	picanhaBurger,
+	rolledMignon,
+	couple20,
+	fishAndChips,
+	truffleShrimpRisotto,
 ];
 
 export const desserts = [
